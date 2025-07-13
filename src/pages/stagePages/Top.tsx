1e5ce4afbcde0f690ee049d0e59838014c24d676
macka-long <310yuta1105@gmail.com>
2025-07-13 15:31:16 +0900

    ステージのトップメニューの大きさ修正

--- src/pages/stagePages/Top.tsx
+++ src/pages/stagePages/Top.tsx
@@ -1,9 +1,9 @@
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
-import { useHistory } from 'react-router-dom';
-import { PageComponentProps } from '../../types/PageComponentProps';
-import { useStageStore } from '../../store/stageStore';
-import AdNativeWrapper from '../../components/AdNativeWrapper';
-
+import { IonText } from "@ionic/react";
+import { useHistory } from "react-router-dom";
+import { PageComponentProps } from "../../types/PageComponentProps";
+import { useStageStore } from "../../store/stageStore";
+import AdNativeWrapper from "../../components/AdNativeWrapper";
+import SiteMenu from "../../components/SiteMenu";
 
 const Top: React.FC<PageComponentProps> = ({ onNext }) => {
   const history = useHistory();
@@ -12,45 +12,101 @@ const Top: React.FC<PageComponentProps> = ({ onNext }) => {
 
   return (
     <div className="ion-padding">
-      <h2 style={{ marginBottom: '2rem' }}>{siteName}</h2>
+      <h2 style={{ marginBottom: "2rem" }}>{siteName}</h2>
+      <SiteMenu
+        onClickContents={() => history.push("/contents", { siteName })}
+        onClickMyPage={() => {
+          return null;
+        }}
+        onClickTerms={() =>
+          history.push("/terms", {
+            ratePerSecond: stage?.ratePerSecond,
+            siteName: siteName,
+            isStarted: true,
+          })
+        }
+        onClickLogout={() => history.push("/login-page")}
+      ></SiteMenu>
 
-      <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '0.25rem', marginBottom: '1rem' }}>
-        <button >サイトトップ</button>
+      {/* <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          gap: "0.25rem",
+          marginBottom: "1rem",
+        }}
+      >
+        <button>サイトトップ</button>
         <button>マイページ</button>
-        <button onClick={() => history.push('/contents', { siteName })}>コンテンツ一覧</button>
-        <button onClick={() => history.push('/terms', {
-          ratePerSecond: stage?.ratePerSecond,
-          siteName: siteName,
-          isStarted: true
-        })}>利用規約</button>
-        <button style={{ gridColumn: 'span 2' }} onClick={() => history.push('/login-page')}>ログアウト</button>
-      </div>
+        <button onClick={() => history.push("/contents", { siteName })}>
+          コンテンツ一覧
+        </button>
+        <button
+          onClick={() =>
+            history.push("/terms", {
+              ratePerSecond: stage?.ratePerSecond,
+              siteName: siteName,
+              isStarted: true,
+            })
+          }
+        >
+          利用規約
+        </button>
+        <button
+          style={{ gridColumn: "span 2" }}
+          onClick={() => history.push("/login-page")}
+        >
+          ログアウト
+        </button>
+      </div> */}
 
-      <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '1rem' }}>
+      <div
+        style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "1rem" }}
+      >
         {Array.from({ length: 4 }).map((_, i) => (
-          <div key={i} style={{ backgroundColor: '#ddd', height: '100px', border: '1px solid #000' }} />
+          <div
+            key={i}
+            style={{
+              backgroundColor: "#ddd",
+              height: "100px",
+              border: "1px solid #000",
+            }}
+          />
         ))}
       </div>
 
       <AdNativeWrapper />
 
-      <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '1rem' }}>
+      <div
+        style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "1rem" }}
+      >
         {Array.from({ length: 6 }).map((_, i) => (
-          <div key={i} style={{ backgroundColor: '#ddd', height: '100px', border: '1px solid #000' }} />
+          <div
+            key={i}
+            style={{
+              backgroundColor: "#ddd",
+              height: "100px",
+              border: "1px solid #000",
+            }}
+          />
         ))}
       </div>
 
-      <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+      <div style={{ textAlign: "center", marginTop: "3rem" }}>
         <IonText
           color="primary"
-          style={{ textDecoration: 'underline', cursor: 'pointer', fontSize: '0.75rem', paddingBottom: '60px' }}
+          style={{
+            textDecoration: "underline",
+            cursor: "pointer",
+            fontSize: "0.75rem",
+            paddingBottom: "60px",
+          }}
           onClick={() => onNext && onNext()}
         >
           退会する
         </IonText>
       </div>
     </div>
-
   );
 };
 
